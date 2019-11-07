77b257bc344ae65040dc688113b7810fcb829d30
Ronald Huggler <ronald@huggler.com.br>
2019-11-06 22:43:07 -0300

    Hello World!

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import express, { Application } from 'express';
+
+import routes from './routes';
+
+class App {
+    public server: Application;
+
+    public constructor() {
+        this.server = express();
+
+        this.middlewares();
+        this.routes();
+    }
+
+    private middlewares(): void {
+        this.server.use(express.json());
+    }
+
+    private routes(): void {
+        this.server.use(routes);
+    }
+}
+
+export default new App().server;
